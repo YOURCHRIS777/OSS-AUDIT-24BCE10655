b80ed5db7daf006c97fbc39b4e2137593c9428b0
chris <chrisofficial030@gmail.com>
2026-03-31 16:55:14 +0530

    Create Scripts1.sh

---
+++ Scripts1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script 1: System Identity
+# Name: CHRIS ARPAN DAVID 
+# Description: Displays system information of the Linux environment.
+
+echo "=============================="
+echo "        SYSTEM IDENTITY       "
+echo "=============================="
+
+echo "Hostname:"
+hostname
+
+echo ""
+
+echo "Current User:"
+whoami
+
+echo ""
+
+echo "Operating System:"
+uname -a
+
+echo ""
+
+echo "Kernel Version:"
+uname -r
+
+echo ""
+
+echo "System Uptime:"
+uptime
+
+echo ""
+
+echo "Current Date and Time:"
+date
+
+echo "=============================="
